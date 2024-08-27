a4d9d007e02eb4aaac13094e33f9c25f686a1416
Alperen <alperenbasali3@gmail.com>
2024-08-27 23:33:06 +0300

    phpturn

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php 
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "mena";
+
+    $baglanti = mysqli_connect($host,$username,$password,$database);
+
+    if(mysqli_connect_errno() > 0){
+        die("hata: ".mysqli_connect_errno());
+    }
+?>
---
+++ hakkimizda.php
@@ -0,0 +1,10 @@
+<?php include("partials/_header.php") ?>
+<style>
+    body{
+        background-color: rgb(209, 207, 207) !important;
+    }
+</style>
+<body>
+    <?php include("partials/_nav.php")?>
+</body>
+</html>
